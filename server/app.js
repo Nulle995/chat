e1f777d83603397278a20ba67ebdc4ff10417d66
Nulle995 <nullewfn@gmail.com>
2025-03-27 16:44:58 -0300

    Added routers

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@ import cookieParser from "cookie-parser";
 import express, { json } from "express";
 import { createServer } from "node:http";
 import initializeSocketIO from "./websocket.js";
+import { authRouter } from "./routes/auth.routes.js";
+import { chatRouter } from "./routes/chat.routes.js";
+import { messageRouter } from "./routes/message.routes.js";
+import { userRouter } from "./routes/user.routes.js";
 
 const app = express();
 app.use(json());
 app.use(cors());
 app.use(cookieParser());
-
+app.use("auth", authRouter);
+app.use("chat", chatRouter);
+app.use("messages", messageRouter);
+app.use("user", userRouter);
 const server = createServer(app);
 const io = initializeSocketIO(server);
 
