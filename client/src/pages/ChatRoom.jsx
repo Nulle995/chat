abd83c0ef01c3a04287e25c81c91ca121a15022a
Nulle995 <nullewfn@gmail.com>
2025-03-27 23:35:46 -0300

    Websocket connection added

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -2,13 +2,18 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { API } from "../services/api";
 import { UserContext } from "../contexts/userContext";
+import { io } from "socket.io-client";
+
 const ChatRoom = () => {
   const { name } = useParams();
   const [chatRoom, setChatRoom] = useState(null);
   const { user } = useContext(UserContext);
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!user) return;
     const formData = new FormData(e.target);
     const messageData = {
       content: formData.get("content"),
@@ -16,13 +21,37 @@ const ChatRoom = () => {
       username: user.username,
     };
     console.log(messageData);
-    try {
-      const res = await API.post("messages", messageData);
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
+    socket.emit("send message", {
+      room: name,
+      message: formData.get("content"),
+    });
+    // try {
+    //   const res = await API.post("messages", messageData);
+    //   console.log(res);
+    // } catch (e) {
+    //   console.log(e);
+    // }
   };
+
+  useEffect(() => {
+    if (!user) return;
+    const newSocket = io("http://localhost:3001", {
+      withCredentials: true,
+      auth: { username: user.username },
+    });
+    setSocket(newSocket);
+
+    newSocket.emit("join room", name);
+
+    newSocket.on("receive message", ({ username, message }) => {
+      console.log(username, message);
+      setMessages((prev) => [...prev, message]);
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [name, user]);
 
   useEffect(() => {
     const getChatRoom = async () => {
@@ -30,6 +59,7 @@ const ChatRoom = () => {
         const res = await API.get(`chat/${name}`);
         console.log(res);
         setChatRoom(res.data);
+        setMessages(res.data.messages);
       } catch (e) {
         console.log(e);
       }
@@ -44,7 +74,7 @@ const ChatRoom = () => {
             {chatRoom.name}- {chatRoom.owner.username}
           </div>
           <div className="chat-messages">
-            {chatRoom.messages.map((msg) => (
+            {messages.map((msg) => (
               <div>
                 <div>{msg.content}</div> <div>{msg.date}</div>
               </div>
